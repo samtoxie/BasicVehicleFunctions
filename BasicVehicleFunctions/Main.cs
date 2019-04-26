d07b7762179a6cd78ed7288adb61930b5a8be878
samtoxie <samtoxie@live.com>
2019-04-27 01:04:52 +0200

    BVF

--- BasicVehicleFunctions/Main.cs
+++ BasicVehicleFunctions/Main.cs
@@ -1,4 +1,4 @@
-﻿[assembly: Rage.Attributes.Plugin("MenuExample", Author = "Guad / alexguirre", Description = "Example using RAGENativeUI")]
+﻿[assembly: Rage.Attributes.Plugin("BasicVehicleFunctions", Author = "Samtoxie", Description = "Basic Vehicle Functions")]
 
 namespace MenuExample
 {
@@ -9,44 +9,81 @@ namespace MenuExample
 
     using RAGENativeUI;
     using RAGENativeUI.Elements;
+    using BasicVehicleFunctions.Utils;
 
     public static class EntryPoint
     {
         private static GameFiber MenusProcessFiber;
 
         private static UIMenu mainMenu;
+        private static UIMenu DoorMainMenu;
+
         private static UIMenuItem toggleBoot;
+        private static UIMenuItem toggleBonnet;
+        private static UIMenuItem togglelf;
+        private static UIMenuItem togglerf;
+        private static UIMenuItem togglefd;
+        private static UIMenuItem togglefp;
+
+        public static UIMenuItem StartCalloutItem;
 
         private static MenuPool menuPool;
 
         public static void Main()
         {
-            // Create a fiber to process our menus
+
             MenusProcessFiber = new GameFiber(ProcessLoop);
+            Logger.DebugLog("ProcessFiber created");
 
-            // Create the MenuPool to easily process our menus
             menuPool = new MenuPool();
+            Logger.DebugLog("MenuPool created");
 
-            // Create our main menu
             mainMenu = new UIMenu("BVF", "~b~Basic Vehicle Functions");
-
-            // Add our main menu to the MenuPool
-            menuPool.Add(mainMenu);
             mainMenu.MouseControlsEnabled = false;
             mainMenu.AllowCameraMovement = true;
-            // create our items and add them to our main menu
-            toggleBoot = new UIMenuItem("Toggle boot");
-            mainMenu.AddItem(toggleBoot);
+            Logger.DebugLog("Main Menu created");
+
+            menuPool.Add(mainMenu);
+            Logger.DebugLog("Main Menu added to pool");
+
+            DoorMainMenu = new UIMenu("BVF", "~b~Basic Vehicle Functions");
+            DoorMainMenu.MouseControlsEnabled = false;
+            DoorMainMenu.AllowCameraMovement = true;
+            Logger.DebugLog("Door Menu created");
+
+            menuPool.Add(DoorMainMenu);
+            Logger.DebugLog("Door Menu added to pool");
+
+            //add items here
+            toggleBoot = new UIMenuItem("Boot");
+            toggleBonnet = new UIMenuItem("Bonnet");
+            togglelf = new UIMenuItem("Rear Driver");
+            togglerf = new UIMenuItem("Rear Passenger");
+            togglefd = new UIMenuItem("Front Driver");
+            togglefp = new UIMenuItem("Front Passenger");
+
+
+            mainMenu.AddItem(StartCalloutItem = new UIMenuItem("Open/Close Doors", " Open and close specific doors"));
+            mainMenu.BindMenuToItem(DoorMainMenu, StartCalloutItem);
+
+            DoorMainMenu.AddItem(togglefd);
+            DoorMainMenu.AddItem(togglefp);
+            DoorMainMenu.AddItem(togglelf);
+            DoorMainMenu.AddItem(togglerf);
+            DoorMainMenu.AddItem(toggleBoot);
+            DoorMainMenu.AddItem(toggleBonnet);
+            DoorMainMenu.RefreshIndex();
+            DoorMainMenu.OnItemSelect += BootOnItemSelect;
+            DoorMainMenu.OnIndexChange += OnItemChange;
+            Logger.DebugLog("Configured Door Menu");
 
             mainMenu.RefreshIndex();
-
             mainMenu.OnItemSelect += OnItemSelect;
             mainMenu.OnIndexChange += OnItemChange;
+            Logger.DebugLog("Configured Main Menu");
 
-            // Start our process fiber
             MenusProcessFiber.Start();
-
-            // Continue with our plugin... in this example, hibernate to prevent it from being unloaded
+            Logger.DebugLog("ProcessFiber started");
             GameFiber.Hibernate();
         }
 
@@ -54,37 +91,151 @@ namespace MenuExample
         public static void OnItemChange(UIMenu sender, int index)
         {
             sender.MenuItems[index].SetLeftBadge(UIMenuItem.BadgeStyle.None);
+            Logger.DebugLog("ItemChange");
         }
 
 
 
-  
+
         public static void OnItemSelect(UIMenu sender, UIMenuItem selectedItem, int index)
         {
             if (sender != mainMenu) return; // We only want to detect changes from our menu.
             // You can also detect the button by using index
-            else if (selectedItem == toggleBoot)
+            
+        }
+
+        public static void BootOnItemSelect(UIMenu sender, UIMenuItem SelectedItem, int index)
+        {
+            if (sender != DoorMainMenu) return; // We only want to detect changes from our menu.
+            // You can also detect the button by using index
+            else if (SelectedItem == toggleBoot)
             {
                 GameFiber.StartNew(delegate // Start a new fiber if the code sleeps or waits and we don't want to block the MenusProcessFiber
                 {
-                        try
+                    try
+                    {
+                        if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].IsOpen)
                         {
-                            if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].IsOpen)
-                            {
-                             Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].Close(false);
-                            }
-                            if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].IsOpen)
-                            {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].Close(false);
+                        }
+                        if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].IsOpen)
+                        {
                             Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[5].Open(false);
-                            }
                         }
-                        catch (Rage.Exceptions.InvalidHandleableException)
+                    }
+                    catch (Rage.Exceptions.InvalidHandleableException)
+                    {
+                        Game.DisplaySubtitle("Closest vehicle has no boot!", 2500);
+                    }
+                });
+            }
+            else if (SelectedItem == toggleBonnet)
+            {
+                GameFiber.StartNew(delegate // Start a new fiber if the code sleeps or waits and we don't want to block the MenusProcessFiber
+                {
+                    try
+                    {
+                        if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[4].IsOpen)
                         {
-                           Game.DisplaySubtitle("Closest vehicle has no boot!", 2500);
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[4].Close(false);
                         }
+                        if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[4].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[4].Open(false);
+                        }
+                    }
+                    catch (Rage.Exceptions.InvalidHandleableException)
+                    {
+                        Game.DisplaySubtitle("Closest vehicle has no bonnet!", 2500);
+                    }
+                });
+            }
+            else if (SelectedItem == togglelf)
+            {
+                GameFiber.StartNew(delegate // Start a new fiber if the code sleeps or waits and we don't want to block the MenusProcessFiber
+                {
+                    try
+                    {
+                        if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[2].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[2].Close(false);
+                        }
+                        if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[2].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[2].Open(false);
+                        }
+                    }
+                    catch (Rage.Exceptions.InvalidHandleableException)
+                    {
+                        Game.DisplaySubtitle("Closest vehicle has no rear driverside door!", 2500);
+                    }
+                });
+            }
+            else if (SelectedItem == togglerf)
+            {
+                GameFiber.StartNew(delegate // Start a new fiber if the code sleeps or waits and we don't want to block the MenusProcessFiber
+                {
+                    try
+                    {
+                        if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[3].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[3].Close(false);
+                        }
+                        if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[3].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[3].Open(false);
+                        }
+                    }
+                    catch (Rage.Exceptions.InvalidHandleableException)
+                    {
+                        Game.DisplaySubtitle("Closest vehicle has no rear passengerside door!", 2500);
+                    }
+                });
+            }
+            else if (SelectedItem == togglefp)
+            {
+                GameFiber.StartNew(delegate // Start a new fiber if the code sleeps or waits and we don't want to block the MenusProcessFiber
+                {
+                    try
+                    {
+                        if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[1].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[1].Close(false);
+                        }
+                        if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[1].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[1].Open(false);
+                        }
+                    }
+                    catch (Rage.Exceptions.InvalidHandleableException)
+                    {
+                        Game.DisplaySubtitle("Closest vehicle has no front passengerside door!", 2500);
+                    }
+                });
+            }
+            else if (SelectedItem == togglefd)
+            {
+                GameFiber.StartNew(delegate // Start a new fiber if the code sleeps or waits and we don't want to block the MenusProcessFiber
+                {
+                    try
+                    {
+                        if (Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[0].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[0].Close(false);
+                        }
+                        if (!Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[0].IsOpen)
+                        {
+                            Game.LocalPlayer.Character.GetNearbyVehicles(1)[0].Doors[0].Open(false);
+                        }
+                    }
+                    catch (Rage.Exceptions.InvalidHandleableException)
+                    {
+                        Game.DisplaySubtitle("Closest vehicle has no front driverside door!", 2500);
+                    }
                 });
             }
         }
+
 
         // The method that contains a loop to handle our menus
         public static void ProcessLoop()
---
+++ BasicVehicleFunctions/Utils/Logger.cs
@@ -0,0 +1,22 @@
+﻿using Rage;
+
+namespace BasicVehicleFunctions.Utils
+{
+    internal static class Logger
+    {
+        private const string LogPrefix = "BasicVehicleCallouts";
+
+        internal static void Log(string LogLine)
+        {
+            string log = string.Format("[{0}]: {1}", LogPrefix, LogLine);
+
+            Game.LogTrivial(log);
+        }
+
+        internal static void DebugLog(string LogLine)
+        {
+            string log = string.Format("[{0}]: {1}", LogPrefix, LogLine);
+            Game.LogTrivial(log);
+        }
+    }
+}
